d69225acc5d17611e2cf1ac823fda9970e4f77f6
Stephania-Martheyn <stephaniemartheyn@hotmail.com>
2023-04-06 18:58:57 -0500

    complete

--- src/features/quote/Cita.test.tsx
+++ src/features/quote/Cita.test.tsx
@@ -1,56 +1,79 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable indent */
-/* eslint-disable linebreak-style */
 import { render } from "../../test-utils";
-import Cita from "./Cita";
+import Quote from "./Cita";
 import { screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("QuoteComponent", () => {
-    it("should render default quote", () => {
-        render(<Cita />);
-        expect(screen.getByText("No se encontro ninguna cita")).toBeInTheDocument();
-    });
+  it("should render default quote", () => {
+    render(<Quote />);
+    expect(screen.getByText("No se encontro ninguna cita")).toBeInTheDocument();
+  });
 
-    it("should render loading when click the random button", async () => {
-        render(<Cita />);
-        await waitFor(() => {
-            userEvent.click(screen.getByText("Obtener cita aleatoria"));
-        });
-        expect(await screen.findByText("CARGANDO...")).toBeInTheDocument();
+  it("should render loading when click the random button", async () => {
+    render(<Quote />);
+    await waitFor(() => {
+      userEvent.click(screen.getByText("Obtener cita aleatoria"));
     });
+    expect(await screen.findByText("CARGANDO...")).toBeInTheDocument();
+  });
 
-    it("should render loading when click the character button", async () => {
-        render(<Cita />);
-        const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-        userEvent.type(input, "Milhouse");
-        await waitFor(() => {
-            userEvent.click(screen.getByText("Obtener Cita"));
-        });
-        expect(await screen.findByText("CARGANDO...")).toBeInTheDocument();
+  it("should render loading when click the character button", async () => {
+    render(<Quote />);
+    const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
+    userEvent.type(input, "Milhouse Van Houten");
+    await waitFor(() => {
+      userEvent.click(screen.getByText("Obtener Cita"));
     });
+    expect(await screen.findByText("CARGANDO...")).toBeInTheDocument();
+  });
 
-    it("should render character quote when click button with a valid input value", async () => {
-        render(<Cita />);
-        const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-        userEvent.type(input, "Milhouse");
-        await waitFor(() => {
-            userEvent.click(screen.getByText("Obtener Cita"));
-        });
-        expect(
-            await screen.findByText("But my mom says I'm cool.")
-        ).toBeInTheDocument();
+  it("should render character quote when click button with a valid input value", async () => {
+    render(<Quote />);
+    const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
+    userEvent.type(input, "Milhouse Van Houten");
+    await waitFor(() => {
+      userEvent.click(screen.getByText("Obtener Cita"));
     });
+    expect(
+      await screen.findByText("But my mom says I'm cool.")
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByText("Bart Simpson")
+    ).not.toBeInTheDocument();
+  });
 
-    it("should render error when invalid input", async () => {
-        render(<Cita />);
-        const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-        userEvent.type(input, "1");
-        await waitFor(() => {
-            userEvent.click(screen.getByText("Obtener Cita"));
-        });
-        expect(
-            await screen.findByText("Por favor ingrese un nombre válido")
-        ).toBeInTheDocument();
+  it("should render random quote when click button without an input value", async () => {
+    render(<Quote />);
+    userEvent.click(screen.getByText("Obtener cita aleatoria"));
+    expect(
+      await screen.findByText("Thank you. Come again.")
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByText("No se encontro ninguna cita")
+    ).not.toBeInTheDocument();
+  });
+
+  it("should render error when not found character name", async () => {
+    render(<Quote />);
+    const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
+    userEvent.type(input, "Mauricio Boye");
+    await waitFor(() => {
+      userEvent.click(screen.getByText("Obtener Cita"));
     });
+    expect(
+      await screen.findByText("Por favor ingrese un nombre válido")
+    ).toBeInTheDocument();
+  });
+
+  it("should render error when invalid input", async () => {
+    render(<Quote />);
+    const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
+    userEvent.type(input, "1");
+    await waitFor(() => {
+      userEvent.click(screen.getByText("Obtener Cita"));
+    });
+    expect(
+      await screen.findByText("Por favor ingrese un nombre válido")
+    ).toBeInTheDocument();
+  });
 });
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,36 +2,30 @@ import { rest } from "msw";
 
 export const handlers = [
     rest.get("https://thesimpsonsquoteapi.glitch.me/quotes", (req, res, ctx) => {
-        if (req.url.searchParams.get("character") == "Milhouse") {
+        const invalidCharacterTest = req.url.searchParams.get("character") == "Mauricio Boye";
+        const characterQuoteTest = req.url.searchParams.get("character") == "Milhouse Van Houten";
+        if (invalidCharacterTest) {
             return res(
-                ctx.json({
-                    info: {
-
-                    },
-                    results: [
-                        {
-                            quote: "But my mom says I'm cool.",
-                            character: "Milhouse Van Houten",
-                            image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FMilhouseVanHouten.png?1497567513002",
-                            characterDirection: "Right"
-                        }]
-                })
+                ctx.status(403)
+            );
+        }
+        if (characterQuoteTest) {
+            return res(
+                ctx.json([{
+                    quote: "But my mom says I'm cool.",
+                    character: "Milhouse Van Houten",
+                    image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FMilhouseVanHouten.png?1497567513002",
+                    characterDirection: "Right"
+                }])
             );
         }
         return res(
-            ctx.json({
-                info: {
-
-                },
-                results: [
-                    {
-                        quote: "Thank you. Come again.",
-                        character: "Apu Nahasapeemapetilon",
-                        image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
-                        characterDirection: "Left"
-                    }
-                ]
-            })
+            ctx.json([{
+                quote: "Thank you. Come again.",
+                character: "Apu Nahasapeemapetilon",
+                image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+                characterDirection: "Left"
+            }])
         );
     }
     )
